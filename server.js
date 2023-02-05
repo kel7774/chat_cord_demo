e37beaa41ed89e7855a3697310aa9aaf202b13c0
Kelli Landry <kellilandry@Kellis-MacBook-Pro.local>
2023-02-05 15:00:27 -0600

    implement sending & formatting of messages

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages")
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+const botName = 'ChatCord Bot';
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,15 +16,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 // run when a client connects
 io.on('connection', socket => {
   // will connect to the client/user who is connecting
-  socket.emit('message', 'Welcome to Chatcord!');
+  socket.emit('message', formatMessage(botName, "Welcome to ChatCord!"));
 
   //broadcast when a user connects, everybody except the user who is connection
-  socket.broadcast.emit('message', 'A user has joined the chat!');
+  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat!'));
 
   // runs when client disconnects
   socket.on('disconnect', ()  => {
-    io.emit('message', 'A user has left the chat');
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
   });
+
+  // listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', formatMessage('USER', msg));
+  })
 })
 
 const PORT = 3000 || process.env.PORT;
